b2a649d0b1cade00fbfd5499aa717a77e46e1d69
haithamamireh-mac <haithamamireh@gmail.com>
2025-08-10 20:22:55 +0300

    update cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,14 @@ import routes from "./routes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      process.env.CORS_ORIGIN ||
+      "https://apex-legends-showcase.haithamamireh.com",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // API routes
